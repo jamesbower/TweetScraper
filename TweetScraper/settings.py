4e918aa183c399e20d048f3a340a0dd948819eed
James Bower <jamesbower@users.noreply.github.com>
2019-04-03 16:59:15 -0400

    Update settings.py

--- TweetScraper/settings.py
+++ TweetScraper/settings.py
@@ -17,8 +17,8 @@ ITEM_PIPELINES = {
 }
 
 # settings for where to save data on disk
-SAVE_TWEET_PATH = './Data/tweet/'
-SAVE_USER_PATH = './Data/user/'
+SAVE_TWEET_PATH = '/builds/hando/tweetscraper/TweetScraper/logs/'
+SAVE_USER_PATH = '/builds/hando/tweetscraper/TweetScraper/logs/'
 
 # settings for mongodb
 MONGODB_SERVER = "127.0.0.1"
